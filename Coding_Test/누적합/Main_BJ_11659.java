dbd28903977785c1a96788e7ce19a6699964aeac
student <student@M4029>
2024-06-25 14:30:27 +0900

    퀸 경우의 수 솔루션

--- Coding_Test/누적합/Main_BJ_11659.java
+++ Coding_Test/누적합/Main_BJ_11659.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main_BJ_11659_2 {
+public class Main_BJ_11659 {
     public static void main(String[] args)  throws Exception  {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
